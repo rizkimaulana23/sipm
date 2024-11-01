ee82caf4b92bbfdb0f383aaa381055e339be20d1
Rizki Maulana <rizki.maulana23@ui.ac.id>
2024-11-01 16:58:35 +0700

    valid

---
+++ src/main/java/com/menyala/sipm/SipmApplication.java
@@ -0,0 +1,13 @@
+package com.menyala.sipm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipmApplication.class, args);
+	}
+
+}
