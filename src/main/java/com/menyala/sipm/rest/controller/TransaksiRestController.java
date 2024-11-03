a5393488808ec900eb47c40c74a85811307f737e
Rizki Maulana <rizki.maulana23@ui.ac.id>
2024-11-03 10:21:10 +0700

    progress

--- src/main/java/com/menyala/sipm/rest/controller/TransaksiRestController.java
+++ src/main/java/com/menyala/sipm/rest/controller/TransaksiRestController.java
@@ -2,6 +2,7 @@ package com.menyala.sipm.rest.controller;
 
 import com.menyala.sipm.rest.dto.response.BarTransaksiLokasiResponseDTO;
 import com.menyala.sipm.rest.dto.response.FailedBaseResponseDTO;
+import com.menyala.sipm.rest.dto.response.LineTransaksiSemuaPasarResponseDTO;
 import com.menyala.sipm.rest.dto.response.SuccessBaseResponseDTO;
 import com.menyala.sipm.rest.service.TransaksiRestService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,7 @@ public class TransaksiRestController {
     private TransaksiRestService transaksiRestService;
 
     @GetMapping("/transaksi-lokasi")
-    public ResponseEntity<?> LineChartTransaksiTiapHarinyaSemuaPasar() {
+    public ResponseEntity<?> BarChartTransaksiLokasiSemuaPasar() {
         try {
             BarTransaksiLokasiResponseDTO responseDTO = transaksiRestService.barTransaksiLokasi();
             var successBaseResponse = new SuccessBaseResponseDTO<BarTransaksiLokasiResponseDTO>();
@@ -39,4 +40,26 @@ public class TransaksiRestController {
             return new ResponseEntity<>(failedBaseResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping("/transaksi")
+    public ResponseEntity<?> LineChartTransaksiSemuaPasar() {
+        try {
+            LineTransaksiSemuaPasarResponseDTO responseDTO = transaksiRestService.lineTransaksiSemuaPasar();
+            var successBaseResponse = new SuccessBaseResponseDTO<LineTransaksiSemuaPasarResponseDTO>();
+            successBaseResponse.setMessage("Data berhasil diambil");
+            successBaseResponse.setStatus(HttpStatus.OK.value());
+            successBaseResponse.setData(responseDTO);
+            return new ResponseEntity<>(successBaseResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            var failedBaseResponse = new FailedBaseResponseDTO<String>();
+            failedBaseResponse.setMessage(e.getMessage());
+            failedBaseResponse.setTimestamp(new Date());
+            failedBaseResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            failedBaseResponse.setError(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+            failedBaseResponse.setData("Terjadi kesalahan di dalam  server");
+            return new ResponseEntity<>(failedBaseResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
 }
--- src/main/java/com/menyala/sipm/rest/service/TransaksiRestServiceImpl.java
+++ src/main/java/com/menyala/sipm/rest/service/TransaksiRestServiceImpl.java
@@ -1,6 +1,7 @@
 package com.menyala.sipm.rest.service;
 
 import com.menyala.sipm.rest.dto.response.BarTransaksiLokasiResponseDTO;
+import com.menyala.sipm.rest.dto.response.LineTransaksiSemuaPasarResponseDTO;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,12 @@ public class TransaksiRestServiceImpl implements TransaksiRestService {
     @Override
     public BarTransaksiLokasiResponseDTO barTransaksiLokasi() {
         BarTransaksiLokasiResponseDTO response = new BarTransaksiLokasiResponseDTO();
+        return response;
+    }
+
+    @Override
+    public LineTransaksiSemuaPasarResponseDTO lineTransaksiSemuaPasar() {
+        LineTransaksiSemuaPasarResponseDTO response = new LineTransaksiSemuaPasarResponseDTO();
         return response;
     }
 }
