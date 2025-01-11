c38b02ff533926b881226bb3693f32d25a1fc2fb
saadrizky <sadrizki14@gmail.com>
2025-01-11 21:36:21 +0700

    inisialisasi firebase properties

--- src/main/java/com/menyala/sipm/firebaseconfig/AppConfig.java
+++ src/main/java/com/menyala/sipm/firebaseconfig/AppConfig.java
@@ -1,2 +1,11 @@
-package com.menyala.sipm.firebaseconfig;public class AppConfig {
-}
+package com.menyala.sipm.firebaseconfig;
+
+
+    import org.springframework.boot.context.properties.EnableConfigurationProperties;
+    import org.springframework.context.annotation.Configuration;
+
+    @Configuration
+    @EnableConfigurationProperties(FirebaseProperties.class)
+    public class AppConfig {
+    }
+
