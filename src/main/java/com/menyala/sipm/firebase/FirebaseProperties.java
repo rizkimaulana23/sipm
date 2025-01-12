52ae3e571c45726eb5b7da4a1bbb8c8ac6c24714
saadrizky <sadrizki14@gmail.com>
2025-01-12 11:00:14 +0700

    bisa deh, tapi gw disable security github secrets nya

--- src/main/java/com/menyala/sipm/firebase/FirebaseProperties.java
+++ src/main/java/com/menyala/sipm/firebase/FirebaseProperties.java
@@ -8,4 +8,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class FirebaseProperties {
 
+    @Value("${firebase.apiKey}")
+    public String apiKey;
+
+    @Value("${firebase.authDomain}")
+    private String authDomain;
+
+    @Value("${firebase.projectId}")
+    private String projectId;
+
+    @Value("${firebase.storageBucket}")
+    private String storageBucket;
+
+    @Value("${firebase.messagingSenderId}")
+    private String messagingSenderId;
+
+    @Value("${firebase.appId}")
+    private String appId;
+
+    @Value("${firebase.measurementId}")
+    private String measurementId;
+
 }
