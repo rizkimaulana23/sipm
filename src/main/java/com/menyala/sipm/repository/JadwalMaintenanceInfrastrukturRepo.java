7762abcddc0ae9e3f33f58bfaa4af1fb04307d25
saadrizky <sadrizki14@gmail.com>
2025-01-21 10:29:04 +0700

    delete maintenance query

--- src/main/java/com/menyala/sipm/repository/JadwalMaintenanceInfrastrukturRepo.java
+++ src/main/java/com/menyala/sipm/repository/JadwalMaintenanceInfrastrukturRepo.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface JadwalMaintenanceInfrastrukturRepo extends JpaRepository<JadwalMaintenanceInfrastruktur, UUID> {
+    void deleteJadwalMaintenanceInfrastrukturById(UUID id);
 }
